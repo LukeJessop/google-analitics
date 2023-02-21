938a23284c454c50b6c86d01c2f3a12311e2f928
LukeJessop <70823991+LukeJessop@users.noreply.github.com>
2023-02-21 12:27:00 -0700

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -38,6 +38,15 @@ function MyApp({ Component, pageProps }) {
         }}
       />
 
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-WRV6WWP"
+          height="0"
+          width="0"
+          style="display:none;visibility:hidden"
+        ></iframe>
+      </noscript>
+
       <Component {...pageProps} />
     </>
   );
