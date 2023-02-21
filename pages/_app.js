8660158d55fa578f127a03ebc24edcc716a536d5
LukeJessop <70823991+LukeJessop@users.noreply.github.com>
2023-02-21 15:14:12 -0700

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,55 +1,8 @@
 import "../styles/globals.css";
-import { useEffect } from "react";
-import Script from "next/script";
-import { useRouter } from "next/router";
-import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
 
-  return (
-    <>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id=""
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `
-        }}
-      />
-
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-WRV6WWP"
-          height="0"
-          width="0"
-          style={{display:'none', visibility:'hidden'}}
-        ></iframe>
-      </noscript>
-
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
